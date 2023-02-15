fe911e1ee2cff90d7ae575ff351b1ed4ec270ac3
shantanuYadav1001 <shantanuyadav13@gmail.com>
2023-02-15 23:47:44 +0530

    added portfolio

--- mysite/settings.py
+++ mysite/settings.py
@@ -143,3 +143,4 @@ CKEDITOR_UPLOAD_PATH = "/static/ckeditor/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
